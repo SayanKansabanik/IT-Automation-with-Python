da3809919d8af4c430b887ac483e56017768e546
Sayan029 <104121376+SayanKansabanik@users.noreply.github.com>
2022-06-27 13:43:56 +0530

    Add files via upload

---
+++ Python Crash Course/hello.py
@@ -0,0 +1,4 @@
+print("hello")
+list = [1,2,3,4,5]
+for i in list:
+    print(list[3], end =" ")
---
+++ Python Crash Course/month_days.py
@@ -0,0 +1,13 @@
+# REPLACE THIS STARTER CODE WITH YOUR FUNCTION
+#june_days = 30
+#print("June has " + str(june_days) + " days.")
+#july_days = 31
+#print("July has " + str(july_days) + " days.")
+
+def month_days(month_name, num_days):
+    print(month_name + " has " + str(num_days)+ " days.")
+
+month_days("january", 31)
+month_days("july", 31)
+month_days("september", 30)
+month_days("may",30)
